1142faac7b5d9b6cb7071775ac7f52409b05f7c5
pjiang604 <patriciaejiang@gmail.com>
2024-03-11 10:11:12 -0700

    formatting

--- index.js
+++ index.js
@@ -4,24 +4,24 @@ import axios from "axios";
 const app = express();
 const port = 3000;
 
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", async (req, res) => {
     try {
-      const response = await axios.get("https://ghibliapi.vercel.app/films");
-      const result = response.data;
-      console.log(result);
-      res.render("movies.ejs", { data: result }); 
+        const response = await axios.get("https://ghibliapi.vercel.app/films");
+        const result = response.data;
+        console.log(result);
+        res.render("movies.ejs", { data: result });
     } catch (error) {
-      console.error("Failed to make request:", error.message);
-      res.render("movies.ejs", {
-        error: error.message,
-      });
+        console.error("Failed to make request:", error.message);
+        res.render("movies.ejs", {
+            error: error.message,
+        });
     }
-  });
+});
 
 
 app.listen(port, () => {
     console.log(`Successfully started server on port ${port}.`);
-  });
+});
 
