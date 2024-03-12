23b0195577a172d697e2efa2543398b784281b4f
pjiang604 <patriciaejiang@gmail.com>
2024-03-12 09:53:24 -0700

    update csv with all movies and remove api

--- index.js
+++ index.js
@@ -19,19 +19,19 @@ db.connect
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static("public"));
 
-app.get("/", async (req, res) => {
-    try {
-        const response = await axios.get("https://ghibliapi.vercel.app/films");
-        const result = response.data;
-        console.log(result);
-        res.render("movies.ejs", { data: result });
-    } catch (error) {
-        console.error("Failed to make request:", error.message);
-        res.render("movies.ejs", {
-            error: error.message,
-        });
-    }
-});
+// app.get("/", async (req, res) => {
+//     try {
+//         const response = await axios.get("https://ghibliapi.vercel.app/films");
+//         const result = response.data;
+//         console.log(result);
+//         res.render("movies.ejs", { data: result });
+//     } catch (error) {
+//         console.error("Failed to make request:", error.message);
+//         res.render("movies.ejs", {
+//             error: error.message,
+//         });
+//     }
+// });
 
 
 app.listen(port, () => {
